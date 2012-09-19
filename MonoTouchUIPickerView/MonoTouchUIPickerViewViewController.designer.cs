ee0fef7ef686bf558fe1db87d54af22573f95110
Christopher House <chris.house.00@gmail.com>
2012-09-19 14:28:33 -0500

    Added text + label to xib

--- MonoTouchUIPickerView/MonoTouchUIPickerViewViewController.designer.cs
+++ MonoTouchUIPickerView/MonoTouchUIPickerViewViewController.designer.cs
@@ -1,3 +1,4 @@
+// WARNING
 //
 // This file has been generated automatically by MonoDevelop to store outlets and
 // actions made in the Xcode designer. If it is removed, they will be lost.
@@ -10,8 +11,15 @@ namespace MonoTouchUIPickerView
 	[Register ("MonoTouchUIPickerViewViewController")]
 	partial class MonoTouchUIPickerViewViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UITextField ColorTextField { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (ColorTextField != null) {
+				ColorTextField.Dispose ();
+				ColorTextField = null;
+			}
 		}
 	}
 }
